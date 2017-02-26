19306e33ae7d7acd512c86b21224f68379b19601
Thomas Walzer <Thomas.Walzer@integratix.net>
2017-02-26 12:17:10 +0100

    updated to 2.2 added addForcedEncoding

--- src/test/java/com/hl7integration/hapi/tests/SetRepetitionsTerserTest.java
+++ src/test/java/com/hl7integration/hapi/tests/SetRepetitionsTerserTest.java
@@ -1,27 +1,35 @@
 package com.hl7integration.hapi.tests;
 
+import ca.uhn.hl7v2.DefaultHapiContext;
 import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.HapiContext;
 import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.parser.ParserConfiguration;
 import ca.uhn.hl7v2.parser.PipeParser;
 import ca.uhn.hl7v2.util.Terser;
 import org.junit.Test;
+import org.relaxng.datatype.ValidationContext;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
 
 
 public class SetRepetitionsTerserTest {
 
 @Test
-public void testSetManualRepetitions(){
+public void testSetManualRepetitions() {
     try {
-     String m = "MSH|^~\\&|hl7Integration|hl7Integration|||||ADT^A01|||2.3|\r" +
+        String m = "MSH|^~\\&|hl7Integration|hl7Integration|||||ADT^A01|||2.3|\r" +
                 "EVN|A01|20130617154644\r" +
                 "PID|1|465 306 5961||407623|Wood^Patrick^^^MR||19700101|1||||||||||\r" +
                 "PV1|1||Location||||||||||||||||261938_6_201306171546|||||||||||||||||||||||||20130617134644|||||||||";
 
-        //Create the Terser
-        PipeParser pipeParser = new PipeParser();
+        HapiContext hc = new DefaultHapiContext();
+        ExecutorService es = hc.getExecutorService(); // to avoid npe when closing context should be fixed https://sourceforge.net/p/hl7api/bugs/223/
+        ParserConfiguration pc = hc.getParserConfiguration();
+        PipeParser pipeParser = hc.getPipeParser();
         Message message = pipeParser.parse(m);
         Terser terser = new Terser(message);
-
         //Add first Address
         terser.set("/.PID-11(0)-1", "13 Oxford Road");
         terser.set("/.PID-11(0)-3", "Oxford");
@@ -29,16 +37,18 @@ public void testSetManualRepetitions(){
         terser.set("/.PID-11(1)-1", "16 London Road");
         terser.set("/.PID-11(1)-3", "London");
 
-        System.out.println(message.encode());
-
+        pc.addForcedEncode("PID-26-1");
+        System.out.println(message.encode().replaceAll("\r", "\r\n"));
+        hc.close();
     } catch (HL7Exception e) {
         e.printStackTrace();
+    } catch (IOException e) {
+        e.printStackTrace();
     }
-
 }
 
-@Test
-public void testSetLoopRepetitions(){
+    @Test
+public void testSetLoopRepetitions() {
     try {
         String m = "MSH|^~\\&|hl7Integration|hl7Integration|||||ADT^A01|||2.3|\r" +
                 "EVN|A01|20130617154644\r" +
