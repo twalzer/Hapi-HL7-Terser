cf9356b498ff9d8f2bf7f072874517a59d136c75
Thomas Walzer <thomas.walzer@integratix.net>
2017-04-21 14:26:29 +0200

    test ohne msh-12 hl7-version

--- src/test/java/com/hl7integration/hapi/tests/SetRepetitionsTerserTest.java
+++ src/test/java/com/hl7integration/hapi/tests/SetRepetitionsTerserTest.java
@@ -4,11 +4,13 @@ import ca.uhn.hl7v2.DefaultHapiContext;
 import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.HapiContext;
 import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.parser.GenericModelClassFactory;
+import ca.uhn.hl7v2.parser.ModelClassFactory;
 import ca.uhn.hl7v2.parser.ParserConfiguration;
 import ca.uhn.hl7v2.parser.PipeParser;
 import ca.uhn.hl7v2.util.Terser;
 import org.junit.Test;
-import org.relaxng.datatype.ValidationContext;
+//import org.relaxng.datatype.ValidationContext;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
@@ -106,6 +108,37 @@ public void testSetLoopRepetitions() {
             terser.set("/.NK1(2)-2-1", "Hall");
             terser.set("/.NK1(2)-2-2", "Anna");
             terser.set("/.NK1(2)-3-1", "Mother");
+
+
+            System.out.println(message.encode());
+
+        } catch (HL7Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void testSetSegmentRepetitionsNoVersionId(){
+        try {
+
+            String m = "MSH|^~\\&|hl7Integration|hl7Integration|||||ADT^A01|msh10|msh11|msh12|20160101102245\r" +
+                    "EVN|A01|20130617154644\r" +
+                    "PID|1|465 306 5961||407623|Wood^Patrick^^^MR||19700101|1||||||||||\r" +
+                    "PV1|1||Location||||||||||||||||261938_6_201306171546|||||||||||||||||||||||||20130617134644|||||||||";
+
+            //Create the Terser
+            final ModelClassFactory modelClassFactory = new GenericModelClassFactory();
+            final PipeParser pipeParser = new PipeParser(modelClassFactory);
+            pipeParser.getParserConfiguration()
+                    .setAllowUnknownVersions(true);
+
+            final Message message = pipeParser.parse(m);
+            Terser terser = new Terser(message);
+
+            //Add first next of Kin
+            //NK1|1|Jones^Joe|Father||||||
+            System.out.println("msh-4: "+terser.get("MSH-4"));
 
 
             System.out.println(message.encode());
